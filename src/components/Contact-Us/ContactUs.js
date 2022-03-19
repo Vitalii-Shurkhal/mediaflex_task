9fd20f284f09f9ab627eaa932192c460d86d84c5
Vitalii-Shurkhal <vshurkhal@gmail.com>
2022-03-19 15:50:44 +0100

    bug fixing

--- src/components/Contact-Us/ContactUs.js
+++ src/components/Contact-Us/ContactUs.js
@@ -16,37 +16,46 @@ const ContactUs = () => {
         tumblrUrl: '',
         websiteUrl: '',
         budget: '',
-        assistance: {
-            'Sponsored days': false,
-            'Creative Guidance': false,
-            'Sponsored Posts': false,
-            'App Istalls': false,
-            'Sponsored Video Posts': false,
-            'Something Else': false
-        }
+        assistance: []
+    
+    }
+
+    const intialValuesAssistance = {
+        'Sponsored days': false,
+        'Creative Guidance': false,
+        'Sponsored Posts': false,
+        'App Istalls': false,
+        'Sponsored Video Posts': false,
+        'Something Else': false
+
     }
     const [formValues, setFormValues] = useState(intialValues);
+    const [formValuesAssistance, setFormValuesAssistance] = useState(intialValuesAssistance)
     const [formErrors, setFormErrors] = useState({});
     const [isSubmit, setIsSubmit] = useState(false);
+    const [hideSucces, setHideSucces] = useState(true)
 
 
     const handlerChange = (event) => {
         const { name, value } = event.target;
         setFormValues({ ...formValues, [name]: value });
+        setHideSucces(true);
     }
+
+    
 
     const handlerChecekedChange = (event) => {
+
         const { name, checked } = event.target;
-        const { assistance } = formValues;
-        const addedAssistance = assistance[name] = checked;
-        setFormValues({ ...formValues, [assistance]: addedAssistance });
-    }
-
-
-    const handlerSubmit = (event) => {
-        event.preventDefault();
-        setFormErrors(validateData(formValues));
-        setIsSubmit(true);
+        const addedAssistance = formValuesAssistance[name] = checked;
+        setFormValuesAssistance({ ...formValuesAssistance, [formValuesAssistance]: addedAssistance[name] })
+        const arrValues = [];
+        for (let key in formValuesAssistance) {
+            if (formValuesAssistance[key] === true) {
+                arrValues.push(key)
+            }
+        }
+        setFormValues({ ...formValues, assistance: arrValues })
     }
 
     const removeEmptyValues = useCallback((obj) => {
@@ -61,11 +70,19 @@ const ContactUs = () => {
         return obj
     }, [])
 
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+        setFormErrors(validateData(formValues));
+        setIsSubmit(true);
+    }
+
     useEffect(() => {
         if (Object.keys(formErrors).length === 0 && isSubmit) {
             const dataSummary = removeEmptyValues(formValues);
             sendPostRequest(dataSummary);
             setFormValues(intialValues);
+            setFormValuesAssistance(intialValuesAssistance)
+            setHideSucces(false)
         }
 
     }, [formErrors, isSubmit])
@@ -122,14 +139,14 @@ const ContactUs = () => {
                         <Form.Group className='mb-3' controlId='FirstNameInput' >
                             <Form.Label>*First Name:</Form.Label>
                             <Form.Control type='text' name='firstName' value={formValues.firstName} onChange={handlerChange} />
-                            <p style={{ color: 'red' }}>{formErrors.firstName}</p>
+                            <p>{formErrors.firstName} </p>
                         </Form.Group>
                     </Col>
                     <Col sm={4}>
                         <Form.Group className='mb-3' controlId='LastNameInput'>
                             <Form.Label>*Last Name:</Form.Label>
-                            <Form.Control type='text' name='lastName' value={formValues.lastName} onChange={handlerChange} />
-                            <p style={{ color: 'red' }}>{formErrors.lastName}</p>
+                            <Form.Control type='text' name='lastName' value={formValues.lastName} onChange={handlerChange}  />
+                            <p>{formErrors.lastName}</p>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -138,7 +155,7 @@ const ContactUs = () => {
                         <Form.Group className='mb-3' controlId='CompanyInput' >
                             <Form.Label>*Company:</Form.Label>
                             <Form.Control type='text' name='company' value={formValues.company} onChange={handlerChange} />
-                            <p style={{ color: 'red' }}>{formErrors.company}</p>
+                            <p>{formErrors.company}</p>
                         </Form.Group>
                     </Col>
                     <Col sm={4}>
@@ -150,7 +167,7 @@ const ContactUs = () => {
                                 <option value="Germany">Germany</option>
                                 <option value="France">France</option>
                             </Form.Select>
-                            <p style={{ color: 'red' }}>{formErrors.country}</p>
+                            <p>{formErrors.country}</p>
                         </Form.Group>
                     </Col>
                 </Row>
@@ -159,7 +176,7 @@ const ContactUs = () => {
                         <Form.Group className='mb-3' controlId='EmailInput' >
                             <Form.Label>*Email:</Form.Label>
                             <Form.Control type='email' name='email' value={formValues.email} onChange={handlerChange} placeholder='example@gmail.com' />
-                            <p style={{ color: 'red' }}>{formErrors.email}</p>
+                            <p>{formErrors.email}</p>
                         </Form.Group>
                     </Col>
                     <Col sm={4}>
@@ -187,7 +204,7 @@ const ContactUs = () => {
                     <Col sm={4}>
                         <Form.Group className='mb-3' controlId='TumblrUrlInput' >
                             <Form.Label>What is Your Monthly Advertising Budget?</Form.Label>
-                            <Form.Select name='budget' value={formValues.budget} onChange={handlerChange} >
+                            <Form.Select name='budget' value={formValues.budget} onChange={handlerChange} autoFocus >
                                 <option>Select...</option>
                                 <option value="100">$100</option>
                                 <option value="200">$200</option>
@@ -202,32 +219,40 @@ const ContactUs = () => {
                             <Form.Label>What can we help you with?</Form.Label>
                             <Row className='mb-3'>
                                 <Col>
-                                    <Form.Check id='1' inline type="checkbox" label="Sponsored Days" name="Sponsored days" onChange={handlerChecekedChange} />
+                                    <Form.Check id='1' inline type="checkbox" label="Sponsored Days" name="Sponsored days"  onChange={handlerChecekedChange} checked= {formValuesAssistance['Sponsored days']}/>
                                 </Col>
                                 <Col>
-                                    <Form.Check id='2' inline type="checkbox" label="Creative Guidance" name="Creative Guidance" onChange={handlerChecekedChange} />
-                                </Col>
-                            </Row>
-                            <Row className='mb-3'>
-                                <Col>
-                                    <Form.Check id='3' inline type="checkbox" label="Sponsored Posts" name="Sponsored Posts" onChange={handlerChecekedChange} />
-                                </Col>
-                                <Col>
-                                    <Form.Check id='4' inline type="checkbox" label="App Istalls" name="App Istalls" onChange={handlerChecekedChange} />
+                                    <Form.Check id='2' inline type="checkbox" label="Creative Guidance" name="Creative Guidance" onChange={handlerChecekedChange} checked = {formValuesAssistance['Creative Guidance']} />
                                 </Col>
                             </Row>
                             <Row className='mb-3'>
                                 <Col>
-                                    <Form.Check id='5' inline type="checkbox" label="Sponsored Video Posts" name="Sponsored Video Posts" onChange={handlerChecekedChange} />
+                                    <Form.Check id='3' inline type="checkbox" label="Sponsored Posts" name="Sponsored Posts" onChange={handlerChecekedChange} checked = {formValuesAssistance['Sponsored Posts']} />
                                 </Col>
                                 <Col>
-                                    <Form.Check id='6' inline type="checkbox" label="Something Else" name="Something Else" onChange={handlerChecekedChange} />
+                                    <Form.Check id='4' inline type="checkbox" label="App Istalls" name="App Istalls" onChange={handlerChecekedChange} checked = {formValuesAssistance['App Istalls']} />
+                                </Col>
+                            </Row>
+                            <Row className='mb-3'>
+                                <Col>
+                                    <Form.Check id='5' inline type="checkbox" label="Sponsored Video Posts" name="Sponsored Video Posts" onChange={handlerChecekedChange} checked = {formValuesAssistance['Sponsored Video Posts']} />
+                                </Col>
+                                <Col>
+                                    <Form.Check id='6' inline type="checkbox" label="Something Else" name="Something Else" onChange={handlerChecekedChange}  checked = {formValuesAssistance['Something Else']}/>
                                 </Col>
                             </Row>
                         </Form.Group>
                     </Col>
                 </Row>
-                <Button variant='success' type='submit'>Submit</Button>
+                <Row>
+                    <Col xs ={2}>
+                    <Button variant='success' type='submit'>Submit</Button>
+                    </Col>
+                    <Col className='p-1'>
+                    <p style={{color: 'green'}} hidden = {hideSucces}>Successfully Sended </p>
+                    </Col>
+                </Row>
+                
             </Form>
         </Container>
 
